28849f1b1d9a8ca4d0fe3a62db20fe2cc53ec03d
sorrycc <sorrycc@gmail.com>
2016-09-09 16:46:14 +0800

    upgrade dva to 1.0, and add css animation

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Spin } from 'antd';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import styles from './Posts.less';
 import PostItem from './PostItem';
 
@@ -9,7 +10,13 @@ function Posts({ loading, posts }) {
       <h2 className={styles.title}>Posts</h2>
       <div className={styles.list}>
         <Spin spinning={loading}>
-          { posts.map(post => <PostItem key={post.key} post={post} /> ) }
+          <ReactCSSTransitionGroup
+            transitionName="item"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            { posts.map(post => <PostItem key={post.key} post={post} /> ) }
+          </ReactCSSTransitionGroup>
         </Spin>
       </div>
     </div>
